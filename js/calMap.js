cc5805c41d7bbcdb1016e0a579e70bbc65c95534
Suchang Cao <caosuchang@yeah.net>
2020-03-04 00:19:35 -0500

    hlghligh problmes area

--- js/calMap.js
+++ js/calMap.js
@@ -13,102 +13,99 @@ let calPrecinctsData;
  * center in California
  */
 function initMap() {
-  map = L.map("calMapID").setView([37.0902405, -121.7128906], 6);
-  L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      noWrap: true,
-      minZoom:6,
-      id: "mapbox/streets-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken:
-        "pk.eyJ1IjoiY29zZWNhbnQiLCJhIjoiY2s2dHZqa3lrMDNmZTNlcXB5ajFpemRiYSJ9.y4qDm1J0MJNoFYoHB91meg"
-    }
-  ).addTo(map);
+    map = L.map("calMapID").setView([37.0902405, -121.7128906], 6);
+    L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            noWrap: true,
+            minZoom: 6,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1IjoiY29zZWNhbnQiLCJhIjoiY2s2dHZqa3lrMDNmZTNlcXB5ajFpemRiYSJ9.y4qDm1J0MJNoFYoHB91meg"
+        }
+    ).addTo(map);
 
-  // remove zoom control UI
-  $(".leaflet-control-zoom").css("visibility", "hidden");
+    // remove zoom control UI
+    $(".leaflet-control-zoom").css("visibility", "hidden");
 
-  // setting boundaries so it only show united states
-  map.setMaxBounds(
-    L.latLngBounds(
-      L.latLng(42.837377, -127.327629), //Southwest
-      L.latLng(31.384359, -110.885666) //Northeast
-    )
-  );
+    // setting boundaries so it only show united states
+    map.setMaxBounds(
+        L.latLngBounds(
+            L.latLng(42.837377, -127.327629), //Southwest
+            L.latLng(31.384359, -110.885666) //Northeast
+        )
+    );
 
-  // add geojson to map
+    // add geojson to map
 }
 
 
 // style for the 3 picked states
 function styleCalBorder(feature) {
-  return {
-    weight: 2,
-    opacity: 1,
-    color: "#0000ff",
-    fillColor: "white"
-  };
+    return {
+        weight: 2,
+        opacity: 1,
+        color: "#0000ff",
+        fillColor: "white"
+    };
 }
 
 // style for all other states
 function styleOtherStates(feature) {
     return {
-      fillColor: "gray",
-      weight: 2,
-      opacity: 1,
-      color: "transparent", //Outline color
-      fillOpacity: 0.7
+        fillColor: "gray",
+        weight: 2,
+        opacity: 1,
+        color: "transparent", //Outline color
+        fillOpacity: 0.7
     };
-  }
+}
 
 // style for counties
 function styleCounties(feature) {
-  return {
-    fillColor: "transparent",
-    weight: 0.5,
-    opacity: 1,
-    color: "black", //Outline color
-    fillOpacity: 0.7
-  };
+    return {
+        fillColor: "transparent",
+        weight: 0.5,
+        opacity: 1,
+        color: "black", //Outline color
+        fillOpacity: 0.7
+    };
 }
 
 function onCaliforniaLocal(feature, layer) {
-  //触发的function
-  console.log("mouse in");
-  // var layer = layer.getElementById("mySidenav");
-  layer.on({
-      click: openNav
-  });
+    //触发的function
+    console.log("mouse in");
+    // var layer = layer.getElementById("mySidenav");
+    layer.on({
+        click: openNav
+    });
 }
 
 initMap();
 
-$.getJSON(OTHER_STATES_PATH, data =>{
-    L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(OTHER_STATES_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(MEXICO_BORDER_PATH, data =>{
-  L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(MEXICO_BORDER_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(TEXAS_BORDER_PATH, data =>{
-  L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(TEXAS_BORDER_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(CAL_BORDER_PATH, data =>{
-  calBorderData = L.geoJson(data, { style: styleCalBorder, onEachFeature: onCaliforniaLocal }).addTo(map);
+$.getJSON(CAL_BORDER_PATH, data => {
+    calBorderData = L.geoJson(data, { style: styleCalBorder, onEachFeature: onCaliforniaLocal }).addTo(map);
 });
 
 
 
 // add listener to the show county and show precinct toggles
-document.getElementById("togBtnC").addEventListener("change", function(){
-  if (this.checked){
-    $.getJSON(CAL_COUNTIES_PATH, data =>{
-      calCountiesData =  L.geoJson(data, {style: styleCounties}).addTo(map);
-    });
-  }else{
-    map.removeLayer(calCountiesData);
-  }
+document.getElementById("togBtnC").addEventListener("change", function() {
+    if (this.checked) {
+        $.getJSON(CAL_COUNTIES_PATH, data => {
+            calCountiesData = L.geoJson(data, { style: styleCounties }).addTo(map);
+        });
+    } else {
+        map.removeLayer(calCountiesData);
+    }
 });
--- js/texasMap.js
+++ js/texasMap.js
@@ -13,103 +13,143 @@ let texasPrecinctData;
  * center in Texas
  */
 function initMap() {
-  map = L.map("texasMapID").setView([31.0902405, -98.7128906], 6);
-  L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      noWrap: true,
-      minZoom:6,
-      id: "mapbox/streets-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken:
-        "pk.eyJ1IjoiY29zZWNhbnQiLCJhIjoiY2s2dHZqa3lrMDNmZTNlcXB5ajFpemRiYSJ9.y4qDm1J0MJNoFYoHB91meg"
-    }
-  ).addTo(map);
+    map = L.map("texasMapID").setView([31.0902405, -98.7128906], 6);
+    L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            noWrap: true,
+            minZoom: 6,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1IjoiY29zZWNhbnQiLCJhIjoiY2s2dHZqa3lrMDNmZTNlcXB5ajFpemRiYSJ9.y4qDm1J0MJNoFYoHB91meg"
+        }
+    ).addTo(map);
 
-  // remove zoom control UI
-  $(".leaflet-control-zoom").css("visibility", "hidden");
+    // remove zoom control UI
+    $(".leaflet-control-zoom").css("visibility", "hidden");
 
-  // setting boundaries so it only show united states
-  map.setMaxBounds(
-    L.latLngBounds(
-      L.latLng(37.837377, -108.327629), //Southwest
-      L.latLng(25.384359, -92.885666) //Northeast
-    )
-  );
+    // setting boundaries so it only show united states
+    map.setMaxBounds(
+        L.latLngBounds(
+            L.latLng(37.837377, -108.327629), //Southwest
+            L.latLng(25.384359, -92.885666) //Northeast
+        )
+    );
 
-  // add geojson to map
+    // add geojson to map
 }
 
 
 // style for the 3 picked states
 function styleTexasBorder(feature) {
-  return {
-    weight: 2,
-    opacity: 1,
-    color: "#0000ff",
-    fillColor: "white"
-  };
+    return {
+        weight: 2,
+        opacity: 1,
+        color: "#0000ff",
+        fillColor: "white"
+    };
 }
 
 // style for all other states
 function styleOtherStates(feature) {
     return {
-      fillColor: "gray",
-      weight: 2,
-      opacity: 1,
-      color: "transparent", //Outline color
-      fillOpacity: 0.7
+        fillColor: "gray",
+        weight: 2,
+        opacity: 1,
+        color: "transparent", //Outline color
+        fillOpacity: 0.7
     };
-  }
+}
 
 
 // style for counties
 function styleCounties(feature) {
-  return {
-    fillColor: "transparent",
-    weight: 0.5,
-    opacity: 1,
-    color: "black", //Outline color
-    fillOpacity: 0.7
-  };
+    return {
+        fillColor: "transparent",
+        weight: 0.5,
+        opacity: 1,
+        color: "black", //Outline color
+        fillOpacity: 0.7
+    };
+}
+
+function onTexasLocal(feature, layer) {
+    //触发的function
+    console.log("mouse in texs");
+    // var layer = layer.getElementById("mySidenav");
+    layer.on({
+        click: openNav
+    });
+}
+// var geoJson;
+// var info = L.control();
+
+function highlightLocalFeature(e) {
+    console.log("mouse in");
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: "#33cc33",
+        dashArray: "",
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    // info.update(layer.feature.properties);
+}
+
+// function resetHighlightLocal(e) {
+//     geoJson.resetStyle(e.target);
+//     info.update();
+// }
+
+function onProblemArea(feature, layer) {
+    //触发的function
+    layer.on({
+        mouseover: highlightLocalFeature,
+        // mouseout: resetHighlightLocal
+    });
 }
 
 
 initMap();
 
-$.getJSON(OTHER_STATES_PATH, data =>{
-    L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(OTHER_STATES_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(MEXICO_BORDER_PATH, data =>{
-    L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(MEXICO_BORDER_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(CAL_BORDER_PATH, data =>{
-  L.geoJson(data, {style: styleOtherStates}).addTo(map);
+$.getJSON(CAL_BORDER_PATH, data => {
+    L.geoJson(data, { style: styleOtherStates }).addTo(map);
 });
-$.getJSON(TEXAS_BORDER_PATH, data =>{
-    texasBorderData = L.geoJson(data, {style: styleTexasBorder}).addTo(map);
+$.getJSON(TEXAS_BORDER_PATH, data => {
+    texasBorderData = L.geoJson(data, { style: styleTexasBorder, onEachFeature: onTexasLocal }).addTo(map);
 });
 
 //map.removeLayer(texasCountiesData);
 // show county border data
-$.getJSON(TEXAS_COUNTIES_PATH, data =>{
-  texasCountiesData =  L.geoJson(data, {style: styleCounties}).addTo(map);
+$.getJSON(TEXAS_COUNTIES_PATH, data => {
+    texasCountiesData = L.geoJson(data, { style: styleCounties, onEachFeature: onTexasLocal }).addTo(map);
 });
+// $.getJSON(TEXAS_COUNTIES_PATH, data => {
+//   texasCountiesData = L.geoJson(data, { style: styleCounties, onEachFeature: onTexasLocal }).addTo(map);
+// });
 
 
 // load precinct border when zoomed in
 map.on('zoomend', function() {
-  if (map.getZoom() > 8){
-    if (!map.hasLayer(texasPrecinctData)){
-      $.getJSON(TEXAS_PRECINCT_PATH, data =>{
-        texasPrecinctData =  L.geoJson(data, {style: styleCounties}).addTo(map);
-      });
+    if (map.getZoom() > 8) {
+        if (!map.hasLayer(texasPrecinctData)) {
+            $.getJSON(TEXAS_PRECINCT_PATH, data => {
+                texasPrecinctData = L.geoJson(data, { style: styleCounties, onEachFeature: onProblemArea }).addTo(map);
+            });
+        }
+    } else {
+        if (map.hasLayer(texasPrecinctData)) map.removeLayer(texasPrecinctData);
     }
-  }
-  else {
-    if (map.hasLayer(texasPrecinctData)) map.removeLayer(texasPrecinctData);
-  }
 });
